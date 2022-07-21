da68bc0a9a7c0c34a03fc1eab6aa4c36cb9f2bc4
blakkkky12 <108606315+blakkkky12@users.noreply.github.com>
2022-07-21 19:18:32 +0700

    update config.js

--- config.js
+++ config.js
@@ -44,7 +44,7 @@ global.doc6 = 'application/vnd.android.package-archive'
 global.owner = ['6281365681189'] //ur owner number
 global.ownername = "nazuaff"
 global.ytname = "DYRROTH YT" //ur yt chanel name
-global.socialm = "GitHub: Warikrr" //ur github or insta name
+global.socialm = "GitHub: arrhmdfauzan_" //ur github or insta name
 global.location = "Indonesia, Kalimantan Selatan, Kotabaru" //ur location
 
 //bot bombdy
